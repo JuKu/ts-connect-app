1aafa7a8ff55f980b267fdf65575eadafc187389
Justin Kuenzel <kuenzel.justin@t-online.de>
2022-01-24 23:38:46 +0100

    Improve login form layout

--- lib/presentation/login/login_activity.dart
+++ lib/presentation/login/login_activity.dart
@@ -28,16 +28,40 @@ class LoginActivity extends StatelessWidget {
             ),
           ],
         ),
-        body: const LoginFormWidget(),/*const Center(
+        body: Column(
+          children: [
+            const SizedBox(height: 40),
+            /*const CircleAvatar(
+              radius: 60,
+              backgroundImage: AssetImage("assets/logo/Logo_512.png"),
+            ),*/
+            SizedBox(
+                width: 150,
+                height: 150,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset("assets/logo/Logo_512.png"),
+                  //fit: BoxFit.cover
+                )),
+            const SizedBox(height: 40),
+            const LoginFormWidget()
+          ],
+        ),
+        /*const Center(
           child: Text("test"),
         ),*/
         backgroundColor: Theme.of(context).primaryColor,
-        bottomNavigationBar: Container(
+        bottomNavigationBar: SizedBox(
           height: 50.0,
           //color: Colors.white70,
           child: const Center(
-            child: Text("Copyright ©2022, All Rights Reserved.\nPowered by JuKuSoft\n", style: TextStyle(fontWeight:FontWeight.w300, fontSize: 12.0, color: /*Color(0xFF162A49)*/Colors.white60),)
-          ),
+              child: Text(
+            "Copyright ©2022, All Rights Reserved.\nPowered by JuKuSoft\n",
+            style: TextStyle(
+                fontWeight: FontWeight.w300,
+                fontSize: 12.0,
+                color: /*Color(0xFF162A49)*/ Colors.white60),
+          )),
         ),
       ),
     );
