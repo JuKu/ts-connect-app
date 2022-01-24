80d415b6a8adef527fa7efebb47f4eaa611962a6
Justin Kuenzel <kuenzel.justin@t-online.de>
2022-01-24 03:11:11 +0100

    Add login button

--- lib/presentation/login/widgets/login_form.dart
+++ lib/presentation/login/widgets/login_form.dart
@@ -21,7 +21,55 @@ class LoginFormWidget extends StatelessWidget {
             border: Border.all(
                 color: Colors.grey, width: 2.0, style: BorderStyle.solid),
             borderRadius: BorderRadius.circular(16),
-
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              //const Text("Benutzername:", style: TextStyle(fontSize: 22), ),
+              //const SizedBox(height: 20),
+              const TextField(obscureText: false, decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Benutzername',
+              )),
+              const SizedBox(height: 20),
+              //const Text("Passwort:", style: TextStyle(fontSize: 22), ),
+              const TextField(obscureText: true, decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Passwort',
+              )),
+              const SizedBox(height: 20),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(4),
+                //borderRadius: const BorderRadius.all(2),
+                child: Stack(
+                  children: [
+                    Positioned.fill(
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: <Color>[
+                              Color(0xFF0D47A1),
+                              Color(0xFF1976D2),
+                              Color(0xFF42A5F5),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.all(16.0),
+                        primary: Colors.white,
+                        textStyle: const TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {},
+                      child: const Text('Anmelden'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
