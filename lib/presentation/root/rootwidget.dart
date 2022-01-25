6ad1cf17e2940e84aed60e52427b7e2bc2440c8c
Justin Kuenzel <kuenzel.justin@t-online.de>
2022-01-25 02:49:50 +0100

    Change RootWidget, add chat button

--- lib/presentation/root/rootwidget.dart
+++ lib/presentation/root/rootwidget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ts_connect_app/presentation/pages/settings/before_login/global_settings.dart';
 
 class RootWidget extends StatefulWidget {
   const RootWidget({Key? key}) : super(key: key);
@@ -15,20 +16,30 @@ class _State extends State<RootWidget> {
           title: const Text("TenSing Connect"),
           centerTitle: true,
           leading: const Icon(Icons.home),
-          actions: const [
+          actions: [
+            IconButton(
+                onPressed: () {},
+                //TODO: change the button color to red, if the user was mentioned,
+                //yellow if there are new messages in last 10 minutes (or someone is active),
+                //white, if no new messages are in chat.
+                //TODO: change also the icon, if no new messages are in chat
+                icon: const Icon(Icons.mark_chat_unread_rounded, color: Colors.white,)),
             /*IconButton(
               onPressed: () {},
               icon: const Icon(Icons.call),
             ),*/
-            /*IconButton(
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => const GlobalSettingsWidget()));
-            },
-            icon: const Icon(Icons.more_vert),
-          ),*/
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const GlobalSettingsWidget()));
+              },
+              icon: const Icon(Icons.more_vert),
+            ),
           ],
         ),
-        body: const Placeholder()
-    );;
+        body: const Placeholder());
+    ;
   }
 }
