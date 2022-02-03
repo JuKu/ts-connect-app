7e109bac6f9fdb92c7c598e233f180280184cb7e
Justin Kuenzel <kuenzel.justin@t-online.de>
2022-02-03 22:52:56 +0100

    Add drawer menu

--- lib/presentation/menu/drawer-menu.dart
+++ lib/presentation/menu/drawer-menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ts_connect_app/presentation/shared/logo.dart';
 
 class DrawerMenuWidget extends StatelessWidget {
   const DrawerMenuWidget({Key? key}) : super(key: key);
@@ -13,21 +14,93 @@ class DrawerMenuWidget extends StatelessWidget {
         // Important: Remove any padding from the ListView.
         padding: EdgeInsets.zero,
         children: [
-          const DrawerHeader(
-            decoration: BoxDecoration(
+          DrawerHeader(
+            decoration: const BoxDecoration(
               color: Colors.blue,
             ),
-            child: Text('Drawer Header'),
+            child: Column(
+              children: const [
+                Text('TS Connect'),
+                SizedBox(height: 20),
+                LogoWidget(width: 100, height: 100)
+              ],
+            ),
           ),
           ListTile(
-            title: const Text('Item 1'),
+            leading: const Icon(Icons.person),
+            title: const Text('Profil'),
             onTap: () {
               // Update the state of the app.
               // ...
             },
           ),
           ListTile(
-            title: const Text('Item 2'),
+            leading: const Icon(Icons.note),
+            title: const Text('Notizen'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.account_box),
+            title: const Text('Kontoeinstellungen'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.notifications),
+            title: const Text('Benachrichtigungen'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.support),
+            title: const Text('Support'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.admin_panel_settings),
+            title: const Text('Admin'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.analytics),
+            title: const Text('Statistiken'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.privacy_tip),
+            title: const Text('Datenschutz'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.import_contacts_rounded),
+            title: const Text('Impressum'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text('Logout'),
             onTap: () {
               // Update the state of the app.
               // ...
